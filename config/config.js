aa824d771becb8482bbfa9f70c0313cbcb84b709
Mahfuzon Akhiar <mahfuzon0@gmail.com>
2021-06-19 00:35:45 +0700

    migration table user

--- config/config.js
+++ config/config.js
@@ -1,21 +1,22 @@
 require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": process.env.DD_USERNAME,
+    "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
-    "username": process.env.DD_USERNAME,
+    "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DD_USERNAME,
+    "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
---
+++ migrations/20210618170438-create-user.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      profession: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      avatar: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      role: {
+        type: Sequelize.ENUM,
+        values: ['admin', 'student'],
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+
+    await queryInterface.addConstraint('Users', {
+      fields: ['email'],
+      type: 'unique',
+      name: 'UNIQUE_USERS_EMAL'
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
